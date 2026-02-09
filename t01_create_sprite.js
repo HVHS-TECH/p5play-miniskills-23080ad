d0eecfb83d6aef844e9afe266aa73aebeaa7d89e
23080ad <23080ad@hvhs.school.nz>
2026-02-10 11:47:01 +1300

    test

--- t01_create_sprite.js
+++ t01_create_sprite.js
@@ -6,19 +6,36 @@
 	
 /*******************************************************/
 // setup()
+var width;
+var height;
+var pos_x;
+var pos_y;
+
+
 /*******************************************************/
 function setup() {
 	console.log("setup: ");
+	
+	//draw the canvas
+	cnv = new Canvas(2000, 2000);
 
+	
+	//create a rectangle
+	rectangle = new Sprite(100, 100, 20, 20, 10);
+	rectangle.color = 'green';
+
+	//create a circle
+	circle = new Sprite(200, 200, 10, 10)
 }
 	
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
-	
-}
 
+	//color the bg
+	background('red');
+}
 /*******************************************************/
 //  END OF APP
 /*******************************************************/

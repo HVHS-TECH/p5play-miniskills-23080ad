467d849077742ccd03dc8947591c6746031f79f6
23080ad <23080ad@hvhs.school.nz>
2026-02-17 12:00:26 +1300

    t21 & t22

--- t21_head2Mouse.js
+++ t21_head2Mouse.js
@@ -3,22 +3,22 @@
 // Move sprite towards the mouse' position
 // Written by ???
 /*******************************************************/
-	
+
 /*******************************************************/
 // setup()
 /*******************************************************/
 function setup() {
 	console.log("setup: ");
-	
+
 	//draw the canvas
 	cnv = new Canvas(1000, 1000);
 
 	//create a rectangle
-	rectangle = new Sprite(100, 100, 20, 20);
+	rectangle = new Sprite(100, 100, 20, 20, 'd');
 	rectangle.color = 'green';
 	rectangle.rotationspeed = 2;
 }
-	
+
 /*******************************************************/
 // draw()
 /*******************************************************/
@@ -27,7 +27,10 @@ function draw() {
 	//color the bg
 	background('black');
 
-	rectangle.moveTowards(mouseX,mouseY,p);
+	rectangle.moveTowards(mouseX, mouseY, 1);
+	{
+		rectangle.moveTo(mouseX, mouseY, 5);
+	}
 }
 
 
--- t22_keyboard.js
+++ t22_keyboard.js
@@ -10,14 +10,60 @@
 function setup() {
 	console.log("setup: ");
 
+	//draw the canvas
+	cnv = new Canvas(1000, 1000);
+
+	//create a rectangle
+	rectangle = new Sprite(100, 100, 20, 20, 'd');
+	rectangle.color = 'green';
+	rectangle.rotationspeed = 2;
+	rectangle.friction = 2;
 }
-	
+
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
-	
+
+	//color the bg
+	background('black');
+
+	//movement controls
+	if (kb.pressing('left')){
+		rectangle.vel.x = -5;
+	}
+
+	if (kb.pressing('right')){
+		rectangle.vel.x = 5;
+	}
+
+	if (kb.pressing('up')){
+		rectangle.vel.y = -5;
+	}
+
+	if (kb.pressing('down')){
+		rectangle.vel.y = 5;
+	}
+
+	//stoping controls
+	if (kb.released('left')){
+		rectangle.vel.x = 0;
+	}
+
+	if (kb.released('right')){
+		rectangle.vel.x = 0;
+	}
+
+	if (kb.released('up')){
+		rectangle.vel.y = 0;
+	}
+
+	if (kb.released('down')){
+		rectangle.vel.y = 0;
+	}
+
 }
+
 
 /*******************************************************/
 //  END OF APP
